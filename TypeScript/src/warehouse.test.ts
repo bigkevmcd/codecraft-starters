96ae54dcbcf9e6df04e0236dd7cbbd7a0e5651f7
Kevin McDermott <bigkevmcd@gmail.com>
2023-08-14 06:53:56 +0100

    Add example.

--- TypeScript/src/warehouse.test.ts
+++ TypeScript/src/warehouse.test.ts
@@ -1,63 +1,73 @@
 import * as warehouse from './__fixtures__/warehouse.json';
-
-interface Product {
-  id: string;
-  name: string;
-  size: number;
-}
-
-interface Bin {
-  id: string;
-  sku: string;
-  quantity: number;
-}
-
-interface Aisle {
-  id: string;
-  bins: Bin[];
-}
-
-interface Warehouse {
-  products: Product[];
-  aisles: Aisle[];
-}
-
-type binPredicate = (bin: Bin) => boolean;
-
-function findWarehouseBins(warehouse: Warehouse, pred: binPredicate): any[] {
-  return warehouse.aisles.flatMap((aisle: Aisle) => aisle.bins.filter(pred));
-}
+import { findWarehouseBins } from './warehouse';
 
 describe('findWarehouseBins', () => {
-  it('accepts a warehouse', () => {
-
+  it('finds bins in their aisles', () => {
     const bins = findWarehouseBins(warehouse, (bin: any): boolean => true);
 
     expect(bins).toEqual([
       {
-        id: "1",
-        quantity: 50,
-        sku: "PROD01",
+        aisleId: "1",
+        bin: {
+          id: "1",
+          quantity: 50,
+          sku: "PROD01",
+        },
       },
       {
-        id: "2",
-        quantity: 5,
-        sku: "PROD02",
+        aisleId: "1",
+        bin: {
+          id: "2",
+          quantity: 5,
+          sku: "PROD02",
+        },
       },
       {
-        id: "16",
-        quantity: 0,
-        sku: "PROD03",
+        aisleId: "2",
+        bin: {
+          id: "16",
+          quantity: 0,
+          sku: "PROD03",
+        },
       },
       {
-        id: "17",
-        quantity: 24,
-        sku: "PROD03",
+        aisleId: "2",
+        bin: {
+          id: "17",
+          quantity: 24,
+          sku: "PROD03",
+        },
       },
       {
-        id: "18",
-        quantity: 25,
-        sku: "PROD03",
+        aisleId: "2",
+        bin: {
+          id: "18",
+          quantity: 25,
+          sku: "PROD03",
+        },
+      },
+    ]);
+  });
+
+  it('filters bins', () => {
+    const bins = findWarehouseBins(warehouse, (bin: any): boolean => bin.quantity > 24);
+
+    expect(bins).toEqual([
+      {
+        aisleId: "1",
+        bin: {
+          id: "1",
+          quantity: 50,
+          sku: "PROD01",
+        },
+      },
+      {
+        aisleId: "2",
+        bin: {
+          id: "18",
+          quantity: 25,
+          sku: "PROD03",
+        },
       },
     ]);
   });
